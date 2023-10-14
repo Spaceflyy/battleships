1d0ab6377b0508bacb1cc810abb1c3594581fb94
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-10-14 12:17:46 +0100

    Add function check if all ships are sunk on board

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,22 +3,10 @@ function gameBoard() {
 	const _board = [];
 	const _placedShips = [];
 
-	const getBoard = () => _board;
-
-	const getShip = (x, y) => {
-		for (let i = 0; i < _placedShips.length; i++) {
-			if (_placedShips[i].getName() === _board[x][y].taken) {
-				return i;
-			}
-		}
-	};
-
-	const getPlacedShips = () => _placedShips;
-
 	(function setupBoard() {
-		for (let i = 0; i < 10; i++) {
+		for (let i = 0; i < 10; i += 1) {
 			_board[i] = [];
-			for (let j = 0; j < 10; j++) {
+			for (let j = 0; j < 10; j += 1) {
 				_board[i][j] = {
 					location: [i, j],
 					taken: null,
@@ -27,8 +15,22 @@ function gameBoard() {
 		}
 	})();
 
+	const getBoard = () => _board;
+
+	const getShip = (x, y) => {
+		let index;
+		for (let i = 0; i < _placedShips.length; i += 1) {
+			if (_placedShips[i].getName() === _board[x][y].taken) {
+				index = i;
+			}
+		}
+		return index;
+	};
+
+	const getPlacedShips = () => _placedShips;
+
 	const placeShip = (ship, x, y) => {
-		for (let i = x; i < ship.getLength(); i++) {
+		for (let i = x; i < ship.getLength(); i += 1) {
 			_board[i][y].taken = ship.getName();
 		}
 		_placedShips.push(ship);
@@ -43,11 +45,14 @@ function gameBoard() {
 		}
 	};
 
+	const allShipsSunk = () => _placedShips.every((element) => element.getSunk());
+
 	return {
 		placeShip,
 		getBoard,
 		receiveAttack,
 		getPlacedShips,
+		allShipsSunk,
 	};
 }
 
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -13,22 +13,36 @@ describe("Gameboard tests", () => {
 	test("Places ship on board at correct positon", () => {
 		newGameboard.placeShip(newShip, 0, 0);
 
-		for (let i = 0; i < newShip.getLength(); i++) {
+		for (let i = 0; i < newShip.getLength(); i += 1) {
 			expect(newGameboard.getBoard()[i][0].taken).toBe(newShip.getName());
 		}
 
 		expect(newGameboard.getBoard()[3][0].taken).toBe(null);
 	});
 
-	test("Test if gameboard and ship recieves attacks correctly", () => {
+	test("Gameboard and ship recieves attacks correctly", () => {
 		newGameboard.placeShip(newShip, 0, 0);
 		newGameboard.receiveAttack(0, 0);
 
 		expect(newGameboard.getPlacedShips()[0].getHits()).toBe(1);
 	});
 
-	test("Test if gameboard records misses correctly", () => {
+	test("Gameboard records misses correctly", () => {
 		newGameboard.receiveAttack(5, 5);
 		expect(newGameboard.getBoard()[5][5].taken).toBe("miss");
+	});
+
+	test("Returns true if all ships are sunk", () => {
+		newGameboard.placeShip(newShip, 0, 0);
+		newGameboard.receiveAttack(0, 0);
+		newGameboard.receiveAttack(0, 0);
+		newGameboard.receiveAttack(0, 0);
+		expect(newGameboard.allShipsSunk()).toBe(true);
+	});
+
+	test("Returns false if not all ships are sunk", () => {
+		newGameboard.placeShip(newShip, 0, 0);
+
+		expect(newGameboard.allShipsSunk()).toBe(false);
 	});
 });
